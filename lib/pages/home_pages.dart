d94ff317a7f43ebd9f67800d15e9b13e60e86be8
adrian <cauichadrian04@gmail.com>
2025-04-14 19:13:17 -0600

    modify cards

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:wallet_app_ui/utils/my_card.dart';
 
 class HomePages extends StatefulWidget {
@@ -9,6 +10,8 @@ class HomePages extends StatefulWidget {
 }
 
 class _HomePagesState extends State<HomePages> {
+  // page controller
+  final _controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +54,44 @@ class _HomePagesState extends State<HomePages> {
               height: 25,
             ),
             //cards
-            MyCard(),
+            Container(
+              height: 200,
+              child: PageView(
+                scrollDirection: Axis.horizontal,
+                controller: _controller,
+                children: [
+                  MyCard(
+                    balance: 2546.30,
+                    cardNumber: 58708237,
+                    expiryMonth: 12,
+                    expiryYear: 28,
+                    color: Colors.deepPurple[300],
+                  ),
+                  MyCard(
+                    balance: 10400.00,
+                    cardNumber: 90273462,
+                    expiryMonth: 6,
+                    expiryYear: 28,
+                    color: Colors.blue[300],
+                  ),
+                  MyCard(
+                    balance: 8350.70,
+                    cardNumber: 28174671,
+                    expiryMonth: 9,
+                    expiryYear: 28,
+                    color: Colors.red[300],
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 25),
+
+            SmoothPageIndicator(
+              controller: _controller,
+              count: 3,
+              effect: ExpandingDotsEffect(activeDotColor: Colors.grey.shade700),
+            )
 
             // 3 buttons -> send + pay + bills
 
--- lib/utils/my_card.dart
+++ lib/utils/my_card.dart
@@ -1,49 +1,62 @@
 import 'package:flutter/material.dart';
 
 class MyCard extends StatelessWidget {
-  const MyCard({super.key});
+  final double balance;
+  final int cardNumber;
+  final int expiryMonth;
+  final int expiryYear;
+  final color;
+  const MyCard(
+      {super.key,
+      required this.balance,
+      required this.cardNumber,
+      required this.expiryMonth,
+      required this.expiryYear,
+      this.color});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-          color: Colors.deepPurple[300],
-          borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 10),
-          Text(
-            "Balance",
-            style: TextStyle(color: Colors.white),
-          ),
-          SizedBox(height: 10),
-          Text(
-            '\$ 2,500.00',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Container(
+        width: 300,
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 10),
+            Text(
+              "Balance",
+              style: TextStyle(color: Colors.white),
             ),
-          ),
-          SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              //card number
-              Text(
-                '**** 3567',
-                style: TextStyle(color: Colors.white),
+            SizedBox(height: 10),
+            Text(
+              '\$ ' + balance.toString(),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
               ),
-              //card expiry date
-              Text(
-                '12/25',
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
-          )
-        ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //card number
+                Text(
+                  cardNumber.toString(),
+                  style: TextStyle(color: Colors.white),
+                ),
+                //card expiry date
+                Text(
+                  expiryMonth.toString() + '/' + expiryYear.toString(),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
